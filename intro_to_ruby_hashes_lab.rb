3445944510205fcdb54f303a22295cacc3e53e90
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-20 09:36:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,8 +22,10 @@ end
 
 def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-  monopoly = {:railroads => { pieces: 4, names: {}, rent_in_dollars: {} }
+  monopoly = {:railroads => { pieces: 4, names: {}, rent_in_dollars: {one_piece_owned: 25, } }
      }
+     
+      sets the 1st key of the :rent_in_dollars hash to a symbol, :one_piece_owned, whose value is the integer 25
 end
 
 def monopoly_with_fourth_tier
