7c6bb87828915c06856e39b1776eecbe2c6ca450
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-20 09:38:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,12 +25,6 @@ def monopoly_with_third_tier
   monopoly = {:railroads => { pieces: 4, names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline: {} }, rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200,   } }
      }
      
-      sets the 3rd key of the :rent_in_dollars hash to a symbol, :three_pieces_owned, whose value is the integer 100 (FAILED - 3)
-      sets the 4th key of the :rent_in_dollars hash to a symbol, :four_pieces_owned, whose value is the integer 200 (FAILED - 4)
-      sets the 1st key of the :names hash to a symbol, :reading_railroad, whosevalue is an empty hash (FAILED - 5)
-      sets the 2nd key of the :names hash to a symbol, :pennsylvania_railroad, whose value is an empty hash (FAILED - 6)
-      sets the 3rd key of the :names hash to a symbol, :b_and_o_railroad, whosevalue is an empty hash (FAILED - 7)
-      sets the 4th key of the :names hash to a symbol, :shortline, whose value is an empty hash
 end
 
 def monopoly_with_fourth_tier
